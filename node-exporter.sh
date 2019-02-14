98ade57d6816e52e131e5ec18f556918fb501644
EC2 Default User <ec2-user@ip-10-201-32-117.ap-southeast-1.compute.internal>
2019-02-14 20:29:41 +0000

    updated node exporter script

--- node-exporter.sh
+++ node-exporter.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 echo Removing old version of Node exporter
-systemctl stop node_exporter
+sudo systemctl stop node_exporter
 sudo rm -rf $(which node_exporter)
 
 echo Dowinloading and installing latest version of Node exporter
-
+cd /tmp
 NODE_EXPORTER_VERSION=$(repo="prometheus/node_exporter" && curl --silent "https://api.github.com/repos/$repo/releases/latest" | grep -Po '"tag_name": "v\K.*?(?=")')
-wget https://github.com/prometheus/node_exporter/releases/download/v${NODE_EXPORTER_VERSION}/node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64.tar.gz -O /tmp/node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64.tar.gz
-tar -xzvf /tmp/node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64.tar.gz
-cd /tmp/node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64
+wget https://github.com/prometheus/node_exporter/releases/download/v${NODE_EXPORTER_VERSION}/node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64.tar.gz
+tar -xzvf node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64.tar.gz
+cd node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64
 cp node_exporter /usr/local/bin
 
 # create user
-useradd --no-create-home --shell /bin/false node_exporter
+sudo useradd --no-create-home --shell /bin/false node_exporter
 
-chown node_exporter:node_exporter /usr/local/bin/node_exporter
+sudo chown node_exporter:node_exporter /usr/local/bin/node_exporter
 
 echo '[Unit]
 Description=Node Exporter
@@ -32,9 +32,9 @@ ExecStart=/usr/local/bin/node_exporter
 WantedBy=multi-user.target' > /etc/systemd/system/node_exporter.service
 
 # enable node_exporter in systemctl
-systemctl daemon-reload
-systemctl start node_exporter
-systemctl enable node_exporter
+sudo systemctl daemon-reload
+sudo systemctl start node_exporter
+sudo systemctl enable node_exporter
 
 
 echo "Setup complete.
